bfd190ccbf6f985a5aef072d190843d63af6a7af
Suphakit P <me@suphakit.net>
2023-07-26 12:29:15 +0700

    feat<pages>: index styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import TW_CLASS from "../constants/tailwind_class";
 	import Time from "../modules/clock";
 
 	let date: string | undefined = undefined
@@ -24,15 +25,21 @@
 
 <section>
 	<div class="w-[80%] h-screen mx-auto flex flex-col items-center text-center justify-center">
-		<p>
-			{date}
-		</p>
-		<p>
-			{time}
-		</p>
-		<p>
-			{percent}
-		</p>
+		<div id="counterBox" class="fixed top-0 w-full flex flex-row justify-start text-left ">
+			<div class="bg-living-coral h-[5px] duration-1000 " style={`width: ${percent}%`}></div>
+		</div>
+		{#if time === undefined || date === undefined}
+			<h1 class={`${TW_CLASS.heading.xxxl} !text-5xl`}>
+				Loading...
+			</h1>
+		{:else}
+			<h1 class={`${TW_CLASS.heading.xxxl} !text-7xl !py-2`}>
+				{time}
+			</h1>
+			<h3 class={`${TW_CLASS.heading.xxl} !py-2`}>
+				{date}
+			</h3>
+		{/if}
 	</div>
 </section>
 
